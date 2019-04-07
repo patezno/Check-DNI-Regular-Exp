4ed169a749a81ed3f0f46832d34dd6c0ab9b99c2
Carlos Alhama <patezno97@gmail.com>
2019-04-07 19:55:10 +0200

    caso test para getsize hecho

--- src/test/java/TablaAsignacionTest.java
+++ src/test/java/TablaAsignacionTest.java
@@ -5,6 +5,11 @@ import org.junit.Test;
 public class TablaAsignacionTest {
 
     @Test
+    public void getSizeTest() {
+        Assert.assertEquals(23, TablaAsignacion.getSize());
+    }
+
+    @Test
     public void wordAllowedTest() {
         Assert.assertTrue(TablaAsignacion.wordAllowed("R"));
         Assert.assertTrue(TablaAsignacion.wordAllowed("X"));
